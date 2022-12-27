d24b90cf8daa4a177c8671aa96260fe939e138f2
Sebas Tobias Cordova <120671463+sebastobias@users.noreply.github.com>
2022-12-27 13:53:55 -0300

    Update UserStorage.sol

--- UserStorage.sol
+++ UserStorage.sol
@@ -10,7 +10,6 @@ contract UserStorage {
     struct userInfo {
         string firstName;
         string lastName;
-        string email;
         string username;
         uint48 timeStamp;
     }
@@ -33,10 +32,10 @@ contract UserStorage {
         socialToken = SocialToken(_socialToken);
     }
 
-    mapping(uint256 => mapping(address => userInfo)) public membership;
-    mapping(uint256 => bool) usedIdsMember;
-    mapping(uint256 => bool) usedIdsVip;
-    mapping(address => bool) profileCreated;
+    mapping(address => userInfo) public addressToUserInfo;
+    mapping(uint256 => bool) public usedIdsMember;
+    mapping(uint256 => bool) public usedIdsVip;
+    mapping(address => bool) public profileCreated;
 
     event Claimed(address _to, uint256 _amount);
     event VipClaimed(address _to, uint256 _amount);
@@ -47,105 +46,88 @@ contract UserStorage {
         string memory _username
     ) external {
         require(membershipType(msg.sender) <= 3, "You don't own any NFT");
-        uint256 memId = membershipType(msg.sender);
-        membership[memId][msg.sender].firstName = _firstName;  
-        membership[memId][msg.sender].lastName = _lastName;  
-        membership[memId][msg.sender].username = _username;  
+        addressToUserInfo[msg.sender].firstName = _firstName;  
+        addressToUserInfo[msg.sender].lastName = _lastName;  
+        addressToUserInfo[msg.sender].username = _username;  
         profileCreated[msg.sender] = true;
 
         if(membershipType(msg.sender) == 1) {
-            membership[memId][msg.sender].timeStamp = uint48(block.timestamp);
+            addressToUserInfo[msg.sender].timeStamp = uint48(block.timestamp);
         }
     }
 
     function updateInfo(uint256 _option, string memory _data) external {
-        uint256 memId = membershipType(msg.sender);
         if (_option == 0) {
-        membership[memId][msg.sender].firstName = _data;
+        addressToUserInfo[msg.sender].firstName = _data;
         }
         if (_option == 1) {
-            membership[memId][msg.sender].lastName = _data;
+            addressToUserInfo[msg.sender].lastName = _data;
         }
         if (_option == 2) {
-            membership[memId][msg.sender].email = _data;
-        }
-        if(_option == 3) {
-            membership[memId][msg.sender].username = _data;
+            addressToUserInfo[msg.sender].username = _data;
         }
     }
 
     function deleteMyUser() external {
-        uint256 memId = membershipType(msg.sender);
-        delete membership[memId][msg.sender];
+        delete addressToUserInfo[msg.sender];
+        profileCreated[msg.sender] = true;
     }
 
-    function claim() external {
-        _claimReward(msg.sender);
+    function claimMember(uint256[] calldata tokenIds) external {
+        _claimMember(msg.sender, tokenIds);
+    }
+
+    
+    function claimVip(uint256[] calldata tokenIdsVip, uint256[] calldata tokenIdsMember) external {
+        _claimVip(msg.sender, tokenIdsVip, tokenIdsMember);
     }
 
     function claimPeriodicReward(uint256[] calldata tokenIds) external {
         _claimPeriodicReward(msg.sender, tokenIds);
     }
 
-    function _claimReward(address _address) internal {
-        require(membershipType(_address) <= 2, "You don't have the rights to call this function");
-
-        if (membershipType(_address) == 2 && profileCreated[_address]) {
-            uint256[] memory tokenIds = member.tokensOfOwner(_address);
-            uint256 earned = 0;
-            for(uint256 i = 0; i < tokenIds.length; i++) {
-                require(member.ownerOf(tokenIds[i]) == msg.sender, "Not an owner"); // test if this is neccesary
-                require(!usedIdsMember[tokenIds[i]], "NFT/NFTs already used");
-                usedIdsMember[tokenIds[i]] = true;
-            }  
-            earned += 50 ether * tokenIds.length;
-
-            if (earned > 0) {
-                earned = earned / 10;
-                socialToken.mint(_address, earned);
-                emit Claimed(_address, earned);
-            }
+    function _claimMember(address _address, uint256[] calldata tokenIds) internal {
+        require(membershipType(_address) <= 2, "Not a Member");
+        require(profileCreated[_address], "Not profile found");
+        uint256 earned = 0;
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            require(!usedIdsMember[tokenIds[i]], "NFT/NFTs already used");
+            usedIdsMember[tokenIds[i]] = true;
         }
 
-        if (membershipType(_address) == 1 && profileCreated[_address]) {
-            uint256 earned = 0;
-            uint256[] memory tokenIds = vip.tokensOfOwner(_address);
-            uint256[] memory tokenIdsMember = member.tokensOfOwner(_address);
-            bool memberNftsRewardClaimed = false;
+        earned += 50 ether * tokenIds.length;
 
-            for(uint256 i = 0; i < tokenIds.length; i++) {
-                require(vip.ownerOf(tokenIds[i]) == msg.sender, "Not an owner"); // test if this is neccesary
-                require(!usedIdsVip[tokenIds[i]], "NFT/NFTs already used");
-                usedIdsVip[tokenIds[i]] = true;
-            }  
-            earned += 70 ether * tokenIds.length;
-
-            for(uint256 i = 0; i < tokenIdsMember.length; i++) {
-                memberNftsRewardClaimed = usedIdsMember[tokenIdsMember[i]];
-            }  
-
-            if(!memberNftsRewardClaimed) {
-                earned += 50 ether * member.balanceOf(_address);
-            }
-
-            if (earned > 0) {
-                earned = earned / 10;
-                socialToken.mint(_address, earned);
-                emit Claimed(_address, earned);
-            }
+        if (earned > 0) {
+            earned = earned / 10;
+            socialToken.mint(_address, earned);
+            emit Claimed(_address, earned);
         }
-
     }
+
+    function _claimVip(address _address, uint256[] calldata tokenIdsVip, uint256[] calldata tokenIdsMember) internal {
+        require(membershipType(_address) == 1, "You don't have the rights to call this function");
+        require(profileCreated[_address], "Not profile found");
+        uint256 earned = 0;
+        for (uint256 i = 0; i < tokenIdsVip.length; i++) {
+            require(!usedIdsVip[tokenIdsVip[i]], "NFT/NFTs already used");
+            usedIdsVip[tokenIdsVip[i]] = true;
+        }
+        earned += 75 ether * tokenIdsVip.length;
+        if (earned > 0) {
+            earned = earned / 10;
+            socialToken.mint(_address, earned);
+            emit VipClaimed(_address, earned);
+            if (tokenIdsMember.length != 0) {
+               _claimMember(_address, tokenIdsMember);
+            }
+        }
+    }   
 
     function _claimPeriodicReward(address _address, uint256[] calldata tokenIds) internal {
         require(membershipType(_address) == 1, "You don't have the rights to call this function");
         uint256 earned = 0;
 
-        for(uint256 i = 0; i < tokenIds.length; i++) {
-            require(vip.ownerOf(tokenIds[i]) == msg.sender, "Not an owner"); //test if this is neccesary
-        }
-
-        uint48 createdAt = membership[membershipType(_address)][_address].timeStamp;
+        uint48 createdAt = addressToUserInfo[_address].timeStamp;
         earned += 10 ether * tokenIds.length * (block.timestamp - createdAt) / 1 days;
 
         if (earned > 0) {
@@ -153,14 +135,18 @@ contract UserStorage {
             socialToken.mint(_address, earned);
             emit VipClaimed(_address, earned);
         }
+        addressToUserInfo[_address].timeStamp = uint48(block.timestamp);
     }
 
-    function periodicRewardInfo(address _address, uint256[] calldata tokenIds) external view returns (uint256) { 
+    function periodicRewardInfo(address _address) external view returns (uint256) { 
+        uint48 createdAt = addressToUserInfo[_address].timeStamp;
         require(membershipType(_address) == 1, "You have not the rights to perform this action");
-        uint48 createdAt = membership[membershipType(_address)][_address].timeStamp;
+        require(createdAt > 0, "No timestamp created");
+        require(profileCreated[_address], "No profile created");
+        uint256[] memory tokenIdsVip = vip.tokensOfOwner(_address);
         uint256 earned = 0;
-        earned += 10 ether * tokenIds.length * (block.timestamp - createdAt) / 1 days;
-        return earned;
+        earned += 10 ether * tokenIdsVip.length * (block.timestamp - createdAt) / 1 days;
+        return (earned / 10);
     }
 
     function rewardInfo(address _address) external view returns (uint256) {
@@ -187,7 +173,6 @@ contract UserStorage {
             uint256[] memory tokenIdsVip = vip.tokensOfOwner(_address);
 
             for(uint256 i = 0; i < tokenIdsVip.length; i++) {
-                require(vip.ownerOf(tokenIdsVip[i]) == msg.sender, "Not an owner"); // test if this is necesary
                 require(!usedIdsVip[tokenIdsVip[i]], "NFT/NFTs already used");
             }  
 
